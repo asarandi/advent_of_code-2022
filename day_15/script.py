5cb80d9b09b69ea4d166af9c19fe30e6ed7e0ccb
Alexandr SARANDI <sarandi.alex@gmail.com>
2022-12-15 01:53:41 -0800

    day_15, improve

--- day_15/script.py
+++ day_15/script.py
@@ -1,51 +1,23 @@
 #!/usr/bin/env python3
 
 import re
-from math import inf
-
-
-def manhattan(a: tuple, b: tuple) -> int:
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
 
 # f = "sample.txt"
 # p1_row = 10
 # p2_range = 20
 
-
 f = "input.txt"
 p1_row = 2_000_000
 p2_range = 4_000_000
 
-
-G, D = {}, {}
+G = {}
 for s in open(f).readlines():
     sx, sy, bx, by = map(int, re.findall(r"(-?\d+)", s))
-    src, dst = (sx, sy), (bx, by)
-    G[src], G[dst] = "S", "B"
-    D[src] = manhattan(src, dst)
-
-xmin, ymin, xmax, ymax = inf, inf, -inf, -inf
-for (sx, sy), distance in D.items():
-    xmin = min(sx - distance, xmin)
-    ymin = min(sy - distance, ymin)
-    xmax = max(sx + distance, xmax)
-    ymax = max(sy + distance, ymax)
-
-y, count = p1_row, set()
-for x in range(xmin, xmax + 1):
-    point = (x, y)
-    for sensor, distance in D.items():
-        if manhattan(sensor, point) <= distance:
-            if point not in G:
-                count.add(point)
-                break
-
-print(len(count))
+    G[(sx, sy)] = abs(sx - bx) + abs(sy - by)
 
 for y in range(p2_range):
     intervals = []
-    for (sx, sy), distance in D.items():
+    for (sx, sy), distance in G.items():
         length = abs(sy - y)
         if distance <= length:
             continue
@@ -69,10 +41,15 @@ for y in range(p2_range):
         prev_left, prev_right = merged.pop()
         merged += [(prev_left, max(prev_right, curr_right))]
 
+    if y == p1_row:
+        assert len(merged) == 1
+        left, right = merged.pop()
+        print("part 1:", right - left)
+
     if len(merged) > 1:
         assert len(merged) == 2
         assert (merged[0][1] + 1) == (merged[1][0] - 1)
         x = merged[0][1] + 1
 
-        print(x * 4_000_000 + y)
+        print("part 2:", x * 4_000_000 + y)
         break
